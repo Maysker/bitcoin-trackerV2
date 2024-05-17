d95c73f00d7031072ac80e6d3a789f4ff7c56c3f
Adam Gazdiev <a.gazdiev@gmail.com>
2024-05-17 15:45:35 +0200

    added transactions history to dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransactionHistoryController;
 use App\Http\Controllers\DashboardController;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,7 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::middleware('auth')->get('/profile/transactions', [TransactionHistoryController::class, 'index']);
+    Route::middleware('auth')->get('/profile/transactions', [TransactionHistoryController::class, 'index'])->name('transactions.index');
 
 });
 
