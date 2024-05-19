3038efce64a18d1097cca3357eec3f0d716cb428
Adam Gazdiev <a.gazdiev@gmail.com>
2024-05-19 22:41:53 +0200

    Style Updates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
 @endif
 
     <div style="margin-top: 30px;" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex justify-around p-6">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex justify-between p-6">
             <!-- User Information -->
             <div class="text-gray-900 dark:text-gray-100">
                 <p><strong>Name:</strong> {{ $data['name'] }}</p>
